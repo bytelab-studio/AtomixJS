b814228943a9cdaf2228a8b6ccb71fb8251a6227
Christoph Koschel <christoph.koschel86@gmail.com>
2025-03-22 23:23:49 +0100

    Check for errors in suite

--- tests/utils/node-suit.js
+++ tests/utils/node-suit.js
@@ -1,5 +1,9 @@
-const path = require("path");
+const os = require("os");
 // This file is to create mokup functions of the AtomixJS runtime functions.
+
+const lineBreak = os.platform() == "win32"
+    ? "\r\n"
+    : "\n";
 
 globalThis.print = function (...args) {
     for (const arg of args) {
@@ -10,30 +14,36 @@ globalThis.print = function (...args) {
                     ? _
                     : _.padEnd(_.indexOf(".") + 7, "0");
                 process.stdout.write(_);
-                process.stdout.write("\r\n");
+                process.stdout.write(lineBreak);
                 break;
             case "string":
                 process.stdout.write(arg.padEnd(arg.indexOf(".") + 7, "0"));
-                process.stdout.write("\r\n");
+                process.stdout.write(lineBreak);
                 break;
             case "object":
                 if (arg == null) {
-                    process.stdout.write("null\r\n");
+                    process.stdout.write("null");
+                    process.stdout.write(lineBreak);
                     break;
                 }
-                process.stdout.write("[Object]\r\n");
+                process.stdout.write("[Object]");
+                process.stdout.write(lineBreak);
                 break;
             case "function":
-                process.stdout.write("[Function]\r\n");
+                process.stdout.write("[Function]");
+                process.stdout.write(lineBreak);
                 break;
             case "undefined":
-                process.stdout.write("undefined\r\n");
+                process.stdout.write("undefined");
+                process.stdout.write(lineBreak);
                 break;
             case "boolean":
                 if (arg) {
-                    process.stdout.write("true\r\n");
+                    process.stdout.write("true");
+                    process.stdout.write(lineBreak);
                 } else {
-                    process.stdout.write("false\r\n");
+                    process.stdout.write("false");
+                    process.stdout.write(lineBreak);
                 }
                 break;
         }
@@ -44,3 +54,4 @@ if (process.argv.length < 2) {
 }
 
 require(process.argv[2]);
+const os = require("node:os");
