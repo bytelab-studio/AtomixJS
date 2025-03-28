62abe39c4219f0b498fa4e359e178692d5f96e48
Christoph Koschel <christoph.koschel86@gmail.com>
2025-03-28 11:10:10 +0100

    Simplify assignment expression through tranformers

---
+++ atomixc/src/transform.ts
@@ -0,0 +1,55 @@
+import * as acorn from "acorn";
+import * as esttraverse from "estraverse";
+
+export function transformProgram(program: acorn.Program): void {
+    if (!Array.isArray(program.body)) {
+        return;
+    }
+
+    for (let i = 0; i < program.body.length; i++) {
+        program.body[i] = transformNode(program.body[i]) as any;
+    }
+}
+
+function transformNode(node: acorn.Node): acorn.Node {
+    return esttraverse.replace(node as any, {
+        // @ts-ignore
+        enter(node) {
+            console.log("Enter %s", node.type);
+            if (node.type == "AssignmentExpression") {
+                return transformAssignmentExpression(node as acorn.AssignmentExpression);
+            }
+            return node;
+        },
+        leave(node) {
+            console.log("Leave %s", node.type);
+        },
+    }) as acorn.Node;
+}
+
+function transformAssignmentExpression(node: acorn.AssignmentExpression) {
+    if (node.operator == "=") {
+        return node;
+    }
+
+    const binaryExpression = (<acorn.BinaryExpression>{
+        type: "BinaryExpression",
+        left: node.left,
+        operator: node.operator.substring(0, node.operator.length - 1) as acorn.BinaryOperator,
+        right: node.right,
+        start: node.start,
+        end: node.end,
+        loc: node.loc,
+        range: node.range
+    });
+    return (<acorn.AssignmentExpression>{
+        type: "AssignmentExpression",
+        left: node.left,
+        operator: "=",
+        right: binaryExpression,
+        start: node.start,
+        end: node.end,
+        loc: node.loc,
+        range: node.range
+    });
+}
---
+++ tests/src/transform/assignment/0.js
@@ -0,0 +1,4 @@
+let a = 20;
+print(a);
+a += 20;
+print(a);
