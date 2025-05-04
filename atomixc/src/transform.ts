19398b5ec923513f17dd9bf77c93e5c3439d6538
Christoph Koschel <christoph.koschel86@gmail.com>
2025-05-04 10:27:25 +0200

    Wrap for-update expression in expression statement

--- atomixc/src/transform.ts
+++ atomixc/src/transform.ts
@@ -87,8 +87,17 @@ function transformForStatement(node: acorn.ForStatement) {
         });
 
     const body: acorn.Node = node.update
-        ? packNodes([node.body, node.update])
-        : node.body;
+        ? packNodes([
+            node.body,
+            (<acorn.ExpressionStatement>{
+                type: "ExpressionStatement",
+                expression: node.update,
+                start: node.update.start,
+                end: node.update.end,
+                loc: node.update.loc,
+                range: node.update.range
+            })
+        ]) : node.body;
 
     nodes.push((<acorn.WhileStatement>{
         type: "WhileStatement",
