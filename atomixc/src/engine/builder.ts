88868da48eb6115a375cf7424610c6e34695fdbb
Christoph Koschel <christoph.koschel86@gmail.com>
2025-06-19 20:42:53 +0200

    Fix flag in cdf generation

--- atomixc/src/engine/builder.ts
+++ atomixc/src/engine/builder.ts
@@ -182,7 +182,7 @@ export class EngineBuilder {
         const files: string[] = this.readdirSync(path.join(ENGINE_BASE, "bdwgc")).filter(file => file.endsWith(".c"));
         return files.map(file => ({
             directory: ENGINE_BASE,
-            arguments: this.gateway.compiler.buildCDFArray(path.join(this.objFolder, "bdwgc", path.basename(file) + ".o"), ["-I", path.join(ENGINE_BASE, "bdwgc"), "-I", path.join(ENGINE_BASE, "bdwgc", "private"), "-DENABLE_THREADS=0", "-DGC_DISABLE_INCREMENTAL=1", "-DNO_INCREMENTAL=1", "-DNO_CLOCK=1", "-ULARGE_CONFIG", "-DSTATIC_LINK", "-DGC_NO_GETCONTEXT", "-Wno-macro-redefined"]),
+            arguments: this.gateway.compiler.buildCDFArray(path.join(this.objFolder, "bdwgc", path.basename(file) + ".o"), ["-I", path.join(ENGINE_BASE, "bdwgc"), "-I", path.join(ENGINE_BASE, "bdwgc", "private"), "-DENABLE_THREADS=0", "-DGC_DISABLE_INCREMENTAL=1", "-DNO_INCREMENTAL=1", "-DNO_CLOCK=1", "-ULARGE_CONFIG", "-DSTATIC_LINK", "-DNO_GETCONTEXT", "-Wno-macro-redefined"]),
             file: file
         }));
     }
