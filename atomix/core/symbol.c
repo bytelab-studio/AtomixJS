456159210b9ddf54196ffc1e197e21f58c7b0a6e
Christoph Koschel <christoph.koschel86@gmail.com>
2025-06-24 21:50:51 +0200

    Include api

--- atomix/core/symbol.c
+++ atomix/core/symbol.c
@@ -1,13 +1,14 @@
 #include "symbol.impl.h"
 
 #include "object.h"
+#include "api.h"
 
 #include "value.impl.h"
 
-static JSObject* createSymbol(VM* vm, const char* description)
+static JSObject* createSymbol(VM* vm, char* description)
 {
     JSObject* symbol = object_create_object(object_get_symbol_prototype());
-    object_set_property(vm, symbol, init_string("description"), JS_VALUE_STRING(init_string(description)));
+    object_set_property(vm, symbol, init_string((char*)"description"), JS_VALUE_STRING(init_string(description)));
     return symbol;
 }
 
@@ -17,7 +18,7 @@ JSValue symbol_to_primitive(VM* vm)
 {
     if (!_symbol_to_primitive)
     {
-        _symbol_to_primitive = createSymbol(vm, "Symbol.toPrimitive");
+        _symbol_to_primitive = createSymbol(vm, (char*)"Symbol.toPrimitive");
     }
 
     return JS_VALUE_SYMBOL(_symbol_to_primitive);
