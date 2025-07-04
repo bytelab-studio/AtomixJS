6dfe43ef8e761a107dca0864aa44643244af201a
Christoph Koschel <christoph.koschel86@gmail.com>
2025-07-04 14:13:28 +0200

    Update print function

--- tests/utils/node-suit.js
+++ tests/utils/node-suit.js
@@ -2,44 +2,13 @@ const os = require("os");
 // This file is to create mokup functions of the AtomixJS runtime functions.
 
 globalThis.print = function (...args) {
+    if (args.length == 0) {
+        process.stdout.write("\n");
+        return;
+    }
     for (const arg of args) {
-        switch (typeof arg) {
-            case "number":
-                let _ = arg.toString();
-                _ = _.indexOf(".") == -1
-                    ? _
-                    : _.padEnd(_.indexOf(".") + 7, "0");
-                process.stdout.write(_);
-                process.stdout.write("\n");
-                break;
-            case "string":
-                process.stdout.write(arg);
-                process.stdout.write("\n");
-                break;
-            case "object":
-                if (arg == null) {
-                    process.stdout.write("null\n");
-                    break;
-                }
-                process.stdout.write("[Object]\n");
-                break;
-            case "function":
-                process.stdout.write("[Function]\n");
-                break;
-            case "undefined":
-                process.stdout.write("undefined\n");
-                break;
-            case "boolean":
-                if (arg) {
-                    process.stdout.write("true\n");
-                } else {
-                    process.stdout.write("false\n");
-                }
-                break;
-            case "symbol":
-                process.stdout.write("[Symbol]\n");
-                break;
-        }
+        process.stdout.write(String(arg));
+        process.stdout.write("\n");
     }
 }
 
